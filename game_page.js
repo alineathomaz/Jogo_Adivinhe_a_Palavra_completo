4db4c8945c497663713c43c07eaaa6a2801decaa
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2023-10-26 19:27:33 -0300

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -37,7 +37,7 @@ function send()
 
     	question_word = "<h4 id='word_display'> P. "+remove_charAt3+"</h4>";
     	input_box = "<br>Resposta : <input type='text' id='input_check_box'>";
-   	 check_button = "<br><br><button class='btn btn-info' onclick='check()'>Verificar</button>";
+   	check_button = "<br><br><button class='btn btn-info' onclick='check()'>Verificar</button>";
     	row =  question_word + input_box + check_button ; 
     	document.getElementById("output").innerHTML = row;
 	document.getElementById("word").value = "";
